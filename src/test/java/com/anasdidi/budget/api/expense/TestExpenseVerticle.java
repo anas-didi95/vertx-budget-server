f7957d047adf4640a9f77c6d4bd20c0362dde4c6
Anas Juwaidi <anas.didi95@gmail.com>
2020-11-01 20:11:20 +0800

    Add test to check response status code

---
+++ src/test/java/com/anasdidi/budget/api/expense/TestExpenseVerticle.java
@@ -0,0 +1,37 @@
+package com.anasdidi.budget.api.expense;
+
+import com.anasdidi.budget.MainVerticle;
+import com.anasdidi.budget.common.AppConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.ext.web.client.WebClient;
+
+@ExtendWith(VertxExtension.class)
+public class TestExpenseVerticle {
+
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle(),
+        testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  void testExpenseCreateSuccess(Vertx vertx, VertxTestContext testContext) throws Exception {
+    AppConfig appConfig = AppConfig.instance();
+    WebClient webClient = WebClient.create(vertx);
+
+    webClient.post(appConfig.getAppPort(), appConfig.getAppHost(), "/budget/api/expense").rxSend()
+        .subscribe(response -> {
+          testContext.verify(() -> {
+            Assertions.assertEquals(201, response.statusCode());
+
+            testContext.completeNow();
+          });
+        }, e -> testContext.failNow(e));
+  }
+}
