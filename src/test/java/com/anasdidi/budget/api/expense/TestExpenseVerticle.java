c964745aa759b7c5552b4fa8e4e0a2778cc7bb55
Anas Juwaidi <anas.didi95@gmail.com>
2020-11-01 20:31:54 +0800

    Add test to check response body status object

--- src/test/java/com/anasdidi/budget/api/expense/TestExpenseVerticle.java
+++ src/test/java/com/anasdidi/budget/api/expense/TestExpenseVerticle.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.reactivex.core.Vertx;
@@ -29,6 +30,14 @@ public class TestExpenseVerticle {
         .subscribe(response -> {
           testContext.verify(() -> {
             Assertions.assertEquals(201, response.statusCode());
+
+            JsonObject responseBody = response.bodyAsJsonObject();
+            Assertions.assertNotNull(responseBody);
+
+            JsonObject status = responseBody.getJsonObject("status");
+            Assertions.assertNotNull(status);
+            Assertions.assertEquals(true, status.getBoolean("isSuccess"));
+            Assertions.assertEquals("Record successfully created.", status.getString("message"));
 
             testContext.completeNow();
           });
