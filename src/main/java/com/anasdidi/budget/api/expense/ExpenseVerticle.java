cdc02a45bf9785d95615d25828ffed4851fe4c5a
Anas Juwaidi <anas.didi95@gmail.com>
2020-11-03 20:43:10 +0800

    Refactor code

--- src/main/java/com/anasdidi/budget/api/expense/ExpenseVerticle.java
+++ src/main/java/com/anasdidi/budget/api/expense/ExpenseVerticle.java
@@ -2,9 +2,7 @@ package com.anasdidi.budget.api.expense;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import io.reactivex.Single;
 import io.vertx.core.Promise;
-import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.ext.mongo.MongoClient;
 import io.vertx.reactivex.ext.web.Router;
@@ -13,53 +11,23 @@ public class ExpenseVerticle extends AbstractVerticle {
 
   private static Logger logger = LogManager.getLogger(ExpenseVerticle.class);
   private final Router mainRouter;
-  private final MongoClient mongoClient;
+  private final ExpenseService expenseService;
+  private final ExpenseController expenseController;
 
   public ExpenseVerticle(Router mainRouter, MongoClient mongoClient) {
     this.mainRouter = mainRouter;
-    this.mongoClient = mongoClient;
+    this.expenseService = new ExpenseService(mongoClient);
+    this.expenseController = new ExpenseController(expenseService);
   }
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     Router router = Router.router(vertx);
 
-    router.post("/").handler(routingContext -> {
-      String requestId = routingContext.get("requestId");
-      JsonObject requestBody = routingContext.getBodyAsJson();
-
-      if (logger.isDebugEnabled()) {
-        logger.debug("[debug] requestBody\n{}", requestBody.encodePrettily());
-      }
-
-      Single<JsonObject> responseSubscriber = Single.just(requestBody)//
-          .map(json -> ExpenseVO.fromJson(json))//
-          .flatMap(vo -> {
-            logger.info("[debug] Save vo");
-            return mongoClient.rxSave("expenses", ExpenseVO.toDocument(vo)).toSingle();
-          }).map(id -> {
-            logger.info("[debug] Save success");
-            JsonObject responseBody = new JsonObject()//
-                .put("status", new JsonObject()//
-                    .put("isSuccess", true)//
-                    .put("message", "Record successfully created."))//
-                .put("data", new JsonObject()//
-                    .put("requestId", requestId)//
-                    .put("id", id));
-            return responseBody;
-          });
-
-      responseSubscriber.subscribe(responseBody -> {
-        routingContext.response().putHeader("Content-Type", "application/json").setStatusCode(201)
-            .end(responseBody.encode());
-      }, e -> {
-        routingContext.response().putHeader("Content-Type", "application/json").setStatusCode(500)
-            .end(new JsonObject().put("error", e.getMessage()).encode());
-      });
-    });
+    router.post("/").handler(expenseController::doCreate);
 
     mainRouter.mountSubRouter("/api/expense", router);
-    logger.info("[start] Deployement success");
+    logger.info("[start] Deployment success");
     startPromise.complete();
   }
 }
